7aceee365babfc59a3420b78fa2a379eab5fd0ab
Jeremy O.A Boateng <160321903+nojetlag7@users.noreply.github.com>
2025-03-20 23:33:26 +0000

    todo: add quarterly ranking

--- Log/Arraylab.java
+++ Log/Arraylab.java
@@ -14,16 +14,51 @@ public class Arraylab{
         };
 
         int[] combined_monthly_sales = new int[12];
+        boolean[] greater_month = new boolean[12];
         int[] combined_quarterly_sales = new int[4];
-        int combined_yearly_sales;
-        int counter = 0;
+        int combined_yearly_sales = 0,yearly_hstreet=0,yearly_mall=0;
+        int counter1 = 0,counter2=0;
 
         for(int i = 0;i < hstreet.length;i++){
             for(int j = 0; j < hstreet[i].length;j++){
-                combined_monthly_sales[counter] = hstreet[i][j] + mall[i][j];
-                counter++;
+                combined_monthly_sales[counter1] = hstreet[i][j] + mall[i][j];
+                yearly_hstreet += hstreet[i][j];
+                yearly_mall += mall[i][j];
+                greater_month[counter2] = hstreet[i][j] > mall[i][j]; //boolean array to decide which branch did better monthly
+                counter1++;counter2++;
             }
         }
 
+        for(int i = 0;i< 4;i++){//4 quarters
+            int sum = 0;
+            for(int j = 0;j < 3;j++){//3 months per quarter
+                sum += combined_monthly_sales[i * 3 + j];
+            }
+            combined_quarterly_sales[i] = sum;
+        }
+
+        //yearly sales
+        for(int sales : combined_quarterly_sales){
+            combined_yearly_sales += sales;
+        }
+    
+        System.out.println("Monthly Combined Sales:\n");
+        for (int i = 0; i < combined_monthly_sales.length; i++) {
+            System.out.println("Month " + (i + 1) + ": $" + combined_monthly_sales[i]);
+            System.out.println(greater_month[i] ? "Hstreet Branch had a better month\n" : "Mall branch had a better month\n");
+        }
+
+        System.out.println("\nQuarterly Combined Sales:");
+        for (int i = 0; i < combined_quarterly_sales.length; i++) {
+            System.out.println("Quarter " + (i + 1) + ": $" + combined_quarterly_sales[i]);
+        }
+
+        System.out.println("\nYearly Combined Sales: $" + combined_yearly_sales);
+
+        //ranking the shops
+        System.out.println(yearly_hstreet > yearly_mall ? "Hstreet Branch performed better in the year." : "Mall Branch performed better in the year.");
+       
+
+        
     }
 }
